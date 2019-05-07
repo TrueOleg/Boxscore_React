0493798f87b4ec8e5bf01cf74996a77b05a4690c
trueoleg <trueolegzaichkin@gmail.com>
2019-05-07 17:52:04 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import Nba from './components/nba-component'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Choose league</h1>
+        <div className="btnCont">
+          <button className="navBtn">NBA</button>
+          <button className="navBtn">MLB</button>
+        </div>
+        <Nba></Nba>
       </header>
     </div>
   );
